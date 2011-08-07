ff2330c302ea37fbbcc32326e15aac1f8d73adbb
= <matt@satchamo.com>
2011-08-07 11:28:25 -0700

    Made zero states (0, 1, 2) into constants NEITHER, STARRED, PRIMED, and removed printGrid

--- hungarian.py
+++ hungarian.py
@@ -2,6 +2,11 @@
 # http://csclab.murraystate.edu/bob.pilgrim/445/munkres.html
 import copy
 
+# Zeros in the algorithm can be starred or primed, or neither
+NEITHER = 0
+STARRED = 1
+PRIMED = 2
+
 # Step 1
 # For each row, find the min in that row, and subtract it from every cell in
 # that row 
@@ -18,7 +23,7 @@ def starZeros(grid, grid_info, row_cover, col_cover):
     for r in xrange(len(grid)):
         for c in xrange(len(grid[r])):
             if grid[r][c] == 0 and row_cover[r] == False and col_cover[c] == False:
-                grid_info[r][c] = 1
+                grid_info[r][c] = STARRED
                 row_cover[r] = col_cover[c] = True
 
     uncoverAll(row_cover, col_cover)
@@ -28,7 +33,7 @@ def starZeros(grid, grid_info, row_cover, col_cover):
 def coverColsWithStarredZeros(grid, grid_info, col_cover):
     for r in xrange(len(grid)):
         for c in xrange(len(grid[r])):
-            if grid_info[r][c] == 1:
+            if grid_info[r][c] == STARRED:
                 # cover the column
                 col_cover[c] = True
 
@@ -42,7 +47,7 @@ def coverColsWithStarredZeros(grid, grid_info, col_cover):
 # Return the column of a starred zero in the specified row, or -1
 def isStarInRow(row, grid, grid_info):
     for c in xrange(len(grid[row])):
-        if grid_info[row][c] == 1:
+        if grid_info[row][c] == STARRED:
             return c
 
     return -1
@@ -50,7 +55,7 @@ def isStarInRow(row, grid, grid_info):
 # Return the row of a starred zero in the specified column, or -1
 def isStarInCol(col, grid, grid_info):
     for r in xrange(len(grid)):
-        if grid_info[r][col] == 1:
+        if grid_info[r][col] == STARRED:
             return r
 
     return -1
@@ -74,7 +79,7 @@ def primeZeros(grid, grid_info, row_cover, col_cover):
         if r == -1:
             return False
 
-        grid_info[r][c] = 2
+        grid_info[r][c] = PRIMED
         col_of_star = isStarInRow(r, grid, grid_info)
         if col_of_star == -1:
             return {'row' : r, 'col' : c}
@@ -108,17 +113,17 @@ def augmentPath(grid, grid_info, path):
     for pair in path:
         r = pair['row']
         c = pair['col']
-        if grid_info[r][c] == 1:
-            grid_info[r][c] = 0
+        if grid_info[r][c] == STARRED:
+            grid_info[r][c] = NEITHER
         else:
-            grid_info[r][c] = 1
+            grid_info[r][c] = STARRED
 
 # Remove the prime from every primed item in the path
 def unprimeAll(grid_info):
     for r in xrange(len(grid_info)):
         for c in xrange(len(grid_info[r])):
-            if grid_info[r][c] == 2:
-                grid_info[r][c] = 0
+            if grid_info[r][c] == PRIMED:
+                grid_info[r][c] = NEITHER
 
 # Uncover all the rows and cols
 def uncoverAll(row_cover, col_cover):
@@ -130,13 +135,18 @@ def uncoverAll(row_cover, col_cover):
 # Return the column of a primed zero in the specified row, or -1
 def isPrimeInRow(row, grid, grid_info):
     for c in xrange(len(grid[row])):
-        if grid_info[row][c] == 2:
+        if grid_info[row][c] == PRIMED:
             return c
 
     return -1
 
 # Step 5
-# I really don't want to explain this. I have no idea what its purpose is
+# I really don't want to explain this. I have no idea what its purpose is. Here is the description:
+# Construct a series of alternating primed and starred zeros as follows.  Let Z0 represent the uncovered primed zero found in Step 4.  
+# Let Z1 denote the starred zero in the column of Z0 (if any). Let Z2 denote the primed zero in the row of Z1 (there will always be one).  
+# Continue until the series terminates at a primed zero that has no starred zero in its column.  
+# Unstar each starred zero of the series, star each primed zero of the series, erase all primes and uncover every line in the matrix.  
+# Return to Step 3. 
 def pathStuff(grid, grid_info, row_cover, col_cover, pair):
     path = []
     path.append(pair)
@@ -154,23 +164,6 @@ def pathStuff(grid, grid_info, row_cover, col_cover, pair):
     augmentPath(grid, grid_info, path)
     unprimeAll(grid_info)
     uncoverAll(row_cover, col_cover)
-
-def printGrid(grid, grid_info):
-    for r in xrange(len(grid)):
-        s = []
-        for c in xrange(len(grid[r])):
-            starred = True if grid_info[r][c] == 1 else False
-            primed = True if grid_info[r][c] == 2 else False
-            n = grid[r][c]
-            if starred and primed:
-                s.append("%4d*'" % (n))
-            elif starred:
-                s.append("%4d*" % (n))
-            elif primed:
-                s.append("%4d'" % (n))
-            else:
-                s.append("%4d" % (n))
-        print ",".join(s)
 
 # For every element in the grid, set grid[r][c] = max_value_of_grid - grid[r][c]
 # This is the trick to turn hungarian minimization into maximization
@@ -204,7 +197,7 @@ def assign(grid):
     col_cover = [False]*len(grid[0])
     # all numbers are non starred, and non primed
     # 1 means it's starred, 2 means it's primed
-    grid_info = [[0]*len(grid[0]) for i in grid]
+    grid_info = [[NEITHER]*len(grid[0]) for i in grid]
     # exit when the number of covered zeros equals this
     exit_condition = min(len(grid), len(grid[0])) 
             
@@ -257,7 +250,7 @@ def sum(grid, grid_info):
     sum = 0
     for r in xrange(len(grid_info)):
         for c in xrange(len(grid_info[r])):
-            if grid_info[r][c] == 1:
+            if grid_info[r][c] == STARRED:
                 sum += grid[r][c]
 
     return sum
@@ -273,7 +266,7 @@ def listPoints(grid_info):
     points = []
     for r in xrange(len(grid_info)):
         for c in xrange(len(grid_info[r])):
-            if grid_info[r][c] == 1:
+            if grid_info[r][c] == STARRED:
                 points.append((r, c))
 
     return points
